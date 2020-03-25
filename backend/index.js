8f30a372df2c9cf8ebaa14fba92f14daf92ecd49
Jhonata Rodrigues <jhonata.a.r@hotmail.com>
2020-03-25 17:34:29 -0300

    install nodemon

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (request, response) => {
+    const params = request.query;
+
+    console.log(params);
+
     return response.send('aaaa');
 });
 
