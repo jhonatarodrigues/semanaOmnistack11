ff8fbb5d49aa097d97a20137b8f253d39deaf235
Jhonata Rodrigues <jhonata.a.r@hotmail.com>
2020-03-28 20:35:36 -0300

    finalizacao da semana omnistack11

---
+++ backend/tests/integration/ong.spec.js
@@ -0,0 +1,31 @@
+const request = require('supertest');
+const app = require('../../src/app');
+const connection = require('../../src/database/connection');
+
+describe('ONG', () => {
+    beforeEach(async () => {
+        await connection.migrate.rollback();
+        await connection.migrate.latest();
+    });
+
+
+    afterAll(async () => {
+       await connection.destroy();
+    })
+
+
+    it('should be able to create a new ong', async () => {
+        const response = await request(app)
+            .post('/ongs')
+            .send({
+                name : "Nameeee2",
+                email : "email@email.com",
+                whatsapp : "1995175553",
+                city : "Sao Paulo",
+                uf : "sp"
+            });
+
+        expect(response.body).toHaveProperty('id');
+        expect(response.body.id).toHaveLength(8);
+    });
+});
